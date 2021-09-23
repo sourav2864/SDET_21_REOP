7c7c069e8f219d1ecf75529c1429ecc9daa8cd0a
sourav2864 <sourav2864@gmail.com>
2021-09-23 11:18:05 +0530

    push framework in to git hub

---
+++ ComcastSeleniumFrm/src/test/java/com/comcast/contactTest/ContactTest.java
@@ -0,0 +1,18 @@
+package com.comcast.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test(groups = "smokeTesting")
+	public void createContactTest() {
+		System.out.println("createContactTest is running");
+	}
+	@Test(groups = "regressionTesting")
+	public void editContactTest() {
+		System.out.println("editContactTest is running");
+	}
+	@Test(groups = "regressionTesting")
+	public void deleteContactTest() {
+		System.out.println("deleteContactTest is running");
+	}
+}
